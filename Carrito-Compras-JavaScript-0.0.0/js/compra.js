023a3a086617bf729a4cb5be450b338ae2c60f62
bona <brunobonarelli@gmail.com>
2022-05-01 13:13:07 -0300

    v 2.3

--- Carrito-Compras-JavaScript-0.0.0/js/compra.js
+++ Carrito-Compras-JavaScript-0.0.0/js/compra.js
@@ -39,10 +39,10 @@ function procesarCompra() {
             title: 'Oops...',
             text: 'No hay productos, selecciona alguno',
             showConfirmButton: false,
-            timer: 2000
-        })/* .then(function () {
+            timer: 3000
+        }).then(function () {
             window.location = "index.html";
-        }) */
+        })
     } else if (cliente.value === '' || correo.value === '') {
         Swal.fire({
             type: 'error',
@@ -102,48 +102,3 @@ function procesarCompra() {
         });
     }
 }
-
-
-/* var myform = $("form#procesar-pago");
-
-        myform.submit( (event) => {
-            event.preventDefault();
-
-            // Change to your service ID, or keep using the default service
-            var service_id = "default_service";
-            var template_id = "template_3SA9LsqQ";
-
-
-
-
-
-
-
-            const cargandoGif = document.querySelector('#cargando');
-            cargandoGif.style.display = 'block';
-
-            const enviado = document.createElement('img');
-            enviado.src = 'img/mail.gif';
-            enviado.style.display = 'block';
-            enviado.width = '150';
-
-            emailjs.sendForm(service_id, template_id, myform[0])
-                .then(() => {
-                    cargandoGif.style.display = 'none';
-                    document.querySelector('#loaders').appendChild(enviado);
-
-                    setTimeout(() => {
-                        compra.vaciarLocalStorage();
-                        enviado.remove();
-                        window.location = "index.html";
-                    }, 2000);
-
-
-                }, (err) => {
-                    alert("Error al enviar el email\r\n Response:\n " + JSON.stringify(err));
-                    // myform.find("button").text("Send");
-                });
-
-            return false;
-
-        }); */
